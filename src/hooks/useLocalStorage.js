177f12ecab54880b044751c533db3655b3554a30
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2022-01-12 17:36:23 -0500

    finished adding in both hooks needed and everything is running!

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+const useLocalStorage = (key, initValue) => {
+    const [value, setValue] = useState(() => {
+        if (localStorage.getItem(key)) {
+            return (JSON.parse(localStorage.getItem(key)))
+        } else {
+            localStorage.setItem(key, JSON.stringify(initValue))
+        } return (initValue)
+    });
+
+    const setStoredValue = (newVal) => {
+        localStorage.setItem(key, JSON.stringify(newVal));
+        setValue(newVal);
+    }
+
+    return ([value, setStoredValue])
+
+};
+
+export default useLocalStorage;
